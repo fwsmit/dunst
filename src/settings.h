58bdc61d018e46e7022809ba0028a6e1f2ba46d3
fwsmit <fw.smit01@gmail.com>
2021-04-03 18:03:58 +0200

    settings: Generalize list code for adding more types of lists

--- src/settings.h
+++ src/settings.h
@@ -12,13 +12,15 @@
 #include "notification.h"
 #include "x11/x.h"
 
+#define LIST_END (-1)
+
 enum alignment { ALIGN_LEFT, ALIGN_CENTER, ALIGN_RIGHT };
 enum ellipsize { ELLIPSE_START, ELLIPSE_MIDDLE, ELLIPSE_END };
 enum icon_position { ICON_LEFT, ICON_RIGHT, ICON_OFF };
 enum vertical_alignment { VERTICAL_TOP, VERTICAL_CENTER, VERTICAL_BOTTOM };
 enum separator_color { SEP_FOREGROUND, SEP_AUTO, SEP_FRAME, SEP_CUSTOM };
 enum follow_mode { FOLLOW_NONE, FOLLOW_MOUSE, FOLLOW_KEYBOARD };
-enum mouse_action { MOUSE_NONE, MOUSE_DO_ACTION, MOUSE_CLOSE_CURRENT, MOUSE_CLOSE_ALL, MOUSE_ACTION_END = -1 /* indicates the end of a list of mouse actions  */ };
+enum mouse_action { MOUSE_NONE, MOUSE_DO_ACTION, MOUSE_CLOSE_CURRENT, MOUSE_CLOSE_ALL, MOUSE_ACTION_END = LIST_END /* indicates the end of a list of mouse actions  */ };
 #ifndef ZWLR_LAYER_SHELL_V1_LAYER_ENUM
 #define ZWLR_LAYER_SHELL_V1_LAYER_ENUM
 // Needed for compiling without wayland dependency
